4f0df634d04a044836257c58eafe09aef3a35b22
Sant <109005453+SantiagoYeshua@users.noreply.github.com>
2024-06-23 16:32:37 -0300

    ajuste app.js

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ function alterarStatus(numDoJogo){
         if(mensagemConfirmar == 1){
             imagem.classList.remove('dashboard__item__img--rented');
             botao.classList.remove('dashboard__item__button--return');
-            botao.textContentL = 'Alugar'
+            botao.textContent = 'Alugar'
         }
     } else{
         imagem.classList.add('dashboard__item__img--rented');
